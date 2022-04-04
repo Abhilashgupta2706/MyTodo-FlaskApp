f27ef72667819fe8c759fba1193a59895c5c5252
abhilashgupta2706 <abhilash.gupta.official@gmail.com>
2022-04-04 17:51:16 +0530

    Bug Fixing

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///todo.db"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
@@ -15,27 +15,27 @@ class ToDo(db.Model):
     data_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self) -> str:
-        return f'{self.srno} - {self.title}'
+        return f"{self.srno} - {self.title}"
 
 
-@app.route('/', methods=['GET', 'POST'])
-def HelloWorld():
-    if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
+@app.route("/", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
         todo = ToDo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
 
     allTodo = ToDo.query.all()
-    return render_template('index.html', allTodo=allTodo)
+    return render_template("index.html", allTodo=allTodo)
 
 
-@app.route('/update/<int:srno>', methods=['GET', 'POST'])
+@app.route("/update/<int:srno>", methods=["GET", "POST"])
 def update(srno):
-    if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
+    if request.method == "POST":
+        title = request.form["title"]
+        desc = request.form["desc"]
         todo = ToDo.query.filter_by(srno=srno).first()
         todo.title = title
         todo.desc = desc
@@ -44,16 +44,16 @@ def update(srno):
         return redirect("/")
 
     todo = ToDo.query.filter_by(srno=srno).first()
-    return render_template('update.html', todo=todo)
+    return render_template("update.html", todo=todo)
 
 
-@app.route('/delete/<int:srno>')
+@app.route("/delete/<int:srno>")
 def delete(srno):
     todo = ToDo.query.filter_by(srno=srno).first()
     db.session.delete(todo)
     db.session.commit()
-    return redirect('/')
+    return redirect("/")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run(debug=False, port=2000)
