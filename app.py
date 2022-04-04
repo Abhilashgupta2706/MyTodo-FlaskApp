ef66bce68ee84a4b82fd0d5c49d3335a83faaca1
abhilashgupta2706 <abhilash.gupta.official@gmail.com>
2022-04-04 16:11:35 +0530

    Initial Deploy
    Adding files for first time

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, redirect, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///todo.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+class ToDo(db.Model):
+    srno = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    desc = db.Column(db.String(500), nullable=False)
+    data_created = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self) -> str:
+        return f'{self.srno} - {self.title}'
+
+
+@app.route('/', methods=['GET', 'POST'])
+def HelloWorld():
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = ToDo(title=title, desc=desc)
+        db.session.add(todo)
+        db.session.commit()
+
+    allTodo = ToDo.query.all()
+    return render_template('index.html', allTodo=allTodo)
+
+
+@app.route('/update/<int:srno>', methods=['GET', 'POST'])
+def update(srno):
+    if request.method == 'POST':
+        title = request.form['title']
+        desc = request.form['desc']
+        todo = ToDo.query.filter_by(srno=srno).first()
+        todo.title = title
+        todo.desc = desc
+        db.session.add(todo)
+        db.session.commit()
+        return redirect("/")
+
+    todo = ToDo.query.filter_by(srno=srno).first()
+    return render_template('update.html', todo=todo)
+
+
+@app.route('/delete/<int:srno>')
+def delete(srno):
+    delete = ToDo.query.filter_by(srno=srno).first()
+    db.session.delete(delete)
+    db.session.commit()
+    return redirect('/')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=2001)
